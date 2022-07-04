67de1ccf345a20586dd73ae7ccffe265d93a4bbb
parkyena2 <58115113+parkyena2@users.noreply.github.com>
2022-07-04 18:15:47 +0900

    Create Find_the_Longest_Word_in_a_String.js

---
+++ Find_the_Longest_Word_in_a_String.js
@@ -0,0 +1,15 @@
+/* Return the length of the longest word in the provided sentence.
+
+Your response should be a number. */
+
+
+function findLongestWordLength(str) {
+  let arr = str.split(" ");
+  let longest = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].length > longest) longest = arr[i].length;
+  }
+  return longest;
+}
+
+console.log(findLongestWordLength("The quick brown fox jumped over the lazy dog"));
