f32967c04ccc38317a764ff9b4ec0411ce658ac0
parkyena2 <58115113+parkyena2@users.noreply.github.com>
2022-07-09 13:46:35 +0900

    Create Missing_letters.js

---
+++ intermediateAlgorithmScripting/Missing_letters.js
@@ -0,0 +1,37 @@
+/* Find the missing letter in the passed letter range and return it.
+
+If all letters are present in the range, return undefined.
+
+fearNotLetter("abce") should return the string d.
+fearNotLetter("abcdefghjklmno") should return the string i.
+fearNotLetter("stvwx") should return the string u.
+fearNotLetter("bcdf") should return the string e.
+fearNotLetter("abcdefghijklmnopqrstuvwxyz") should return undefined. */
+
+
+
+
+function fearNotLetter(str) {
+  const correctStr = "abcdefghijklmnopqrstuvwxyz";
+  const arr = [...correctStr];
+  
+  const strArr = [...str];
+  const len = str.length;
+
+  const startIndex = arr.indexOf(strArr[0]);
+  let missingLetter = "";
+  for (let i = 0; i < len; i++) {
+    if (strArr[i] != arr[startIndex + i]) {
+      missingLetter = arr[startIndex + i];
+      break;
+    }
+  }
+
+  if (missingLetter == "") {
+    missingLetter = undefined;
+  }
+
+  return missingLetter;
+}
+
+console.log(fearNotLetter("abce"));
