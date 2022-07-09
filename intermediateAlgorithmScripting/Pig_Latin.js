4b6e729c39504844f9f030a47368830d068243c7
parkyena2 <58115113+parkyena2@users.noreply.github.com>
2022-07-09 12:23:19 +0900

    Create Pig_Latin.js

---
+++ intermediateAlgorithmScripting/Pig_Latin.js
@@ -0,0 +1,35 @@
+/* Pig Latin is a way of altering English Words. The rules are as follows:
+
+- If a word begins with a consonant, take the first consonant or consonant cluster, move it to the end of the word, and add ay to it.
+
+- If a word begins with a vowel, just add way at the end.
+
+Translate the provided string to Pig Latin. Input strings are guaranteed to be English words in all lowercase.
+
+translatePigLatin("california") should return the string aliforniacay.
+translatePigLatin("paragraphs") should return the string aragraphspay.
+translatePigLatin("glove") should return the string oveglay.
+translatePigLatin("algorithm") should return the string algorithmway.
+translatePigLatin("eight") should return the string eightway.
+Should handle words where the first vowel comes in the middle of the word. translatePigLatin("schwartz") should return the string artzschway.
+Should handle words without vowels. translatePigLatin("rhythm") should return the string rhythmay.*/
+
+
+
+
+function translatePigLatin(str) {
+  if ((/^[aeiou]/).test(str)) {
+    str = str.concat("way");
+  } else {
+    let regex = /^[^(aeiou)]+/
+    let consonant = str.match(regex);
+    console.log(consonant);
+    const splitLen = consonant[0].length;
+    str = str.substring(splitLen);
+    str = str.concat(consonant);
+    str = str.concat("ay");
+  }
+  return str;
+}
+
+console.log(translatePigLatin("glove"));
