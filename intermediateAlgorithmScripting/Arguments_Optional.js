25698c4fc5de802bbaaf2af2af0c000fbdc94e2e
parkyena2 <58115113+parkyena2@users.noreply.github.com>
2022-07-22 11:56:37 +0900

    Create Arguments_Optional.js

---
+++ intermediateAlgorithmScripting/Arguments_Optional.js
@@ -0,0 +1,35 @@
+/* Create a function that sums two arguments together. If only one argument is provided, then return a function that expects one argument and returns the sum.
+For example, addTogether(2, 3) should return 5, and addTogether(2) should return a function.
+Calling this returned function with a single argument will then return the sum:
+  var sumTwoAnd = addTogether(2);
+sumTwoAnd(3) returns 5.
+If either argument isn't a valid number, return undefined.
+
+Passed:addTogether(2, 3) should return 5.
+Passed:addTogether(23, 30) should return 53.
+Passed:addTogether(5)(7) should return 12.
+Passed:addTogether("https://www.youtube.com/watch?v=dQw4w9WgXcQ") should return undefined.
+Passed:addTogether(2, "3") should return undefined.
+Passed:addTogether(2)([3]) should return undefined.
+Passed:addTogether("2", 3) should return undefined. */
+
+
+
+function addTogether() {
+  if (arguments.length == 1 && typeof arguments[0] == "number") {
+    const addOne = arguments[0];
+    return function (arg) {
+      if (typeof arg == "number") {
+        return addOne + arg;
+      } else {
+        return undefined;
+      }
+    }
+  } else if (arguments.length == 2 && typeof arguments[0] == "number" && typeof arguments[1] == "number") {
+    return arguments[0] + arguments[1];
+  } else {
+    return undefined;
+  }
+}
+
+console.log(addTogether(2)([3]));
