d77565ced655960b8b9cdb80db3221b1a608abfa
parkyena2 <58115113+parkyena2@users.noreply.github.com>
2022-09-14 20:09:51 +0900

    Update Cash_Register.js
    complete answer

--- JavaScript_Algorithms_and_Data_Structures_Projects/Cash_Register.js
+++ JavaScript_Algorithms_and_Data_Structures_Projects/Cash_Register.js
@@ -1,8 +1,54 @@
+/* Design a cash register drawer function checkCashRegister() that accepts purchase price as the first argument (price), payment as the second argument (cash), and cash-in-drawer (cid) as the third argument.
+
+cid is a 2D array listing available currency.
+
+The checkCashRegister() function should always return an object with a status key and a change key.
+
+Return {status: "INSUFFICIENT_FUNDS", change: []} if cash-in-drawer is less than the change due, or if you cannot return the exact change.
+
+Return {status: "CLOSED", change: [...]} with cash-in-drawer as the value for the key change if it is equal to the change due.
+
+Otherwise, return {status: "OPEN", change: [...]}, with the change due in coins and bills, sorted in highest to lowest order, as the value of the change key.
+
+Currency Unit	Amount
+Penny	$0.01 (PENNY)
+Nickel	$0.05 (NICKEL)
+Dime	$0.1 (DIME)
+Quarter	$0.25 (QUARTER)
+Dollar	$1 (ONE)
+Five Dollars	$5 (FIVE)
+Ten Dollars	$10 (TEN)
+Twenty Dollars	$20 (TWENTY)
+One-hundred Dollars	$100 (ONE HUNDRED)
+See below for an example of a cash-in-drawer array:
+
+[
+  ["PENNY", 1.01],
+  ["NICKEL", 2.05],
+  ["DIME", 3.1],
+  ["QUARTER", 4.25],
+  ["ONE", 90],
+  ["FIVE", 55],
+  ["TEN", 20],
+  ["TWENTY", 60],
+  ["ONE HUNDRED", 100]
+] 
+
+Passed:checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]) should return an object.
+Passed:checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]) should return {status: "OPEN", change: [["QUARTER", 0.5]]}.
+Passed:checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]) should return {status: "OPEN", change: [["TWENTY", 60], ["TEN", 20], ["FIVE", 15], ["ONE", 1], ["QUARTER", 0.5], ["DIME", 0.2], ["PENNY", 0.04]]}.
+Passed:checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]) should return {status: "INSUFFICIENT_FUNDS", change: []}.
+Passed:checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]) should return {status: "INSUFFICIENT_FUNDS", change: []}.
+Passed:checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]) should return {status: "CLOSED", change: [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]}. */
+
+
+
+
 function checkCashRegister(price, cash, cid) {
   let change = cash - price;
   let result = {
     status: "",
-    change: []
+    change: [["PENNY", 0.0], ["NICKEL", 0.0], ["DIME", 0.0], ["QUARTER", 0.0], ["ONE", 0.0], ["FIVE", 0.0], ["TEN", 0.0], ["TWENTY", 0.0], ["ONE HUNDRED", 0.0]]
     };
   let newCid = [...cid];
   let length = newCid.length - 1;
@@ -11,33 +57,41 @@ function checkCashRegister(price, cash, cid) {
   while (length >= 0) {
     if (units[length][1] <= change) {
       while (change - units[length][1] >= 0.0 && newCid[length][1] - units[length][1] >= 0.0) {
-        change = (change - units[length][1]).toFixed(2);
-        newCid[length][1] -= units[length][1];
-        if (result.change.indexOf(units[length][0]) == -1) {
-          result.change.push(units[length]);
-        } else {
-          result.change[length][1] += units[length][1];
-        }
+        change = +(change - units[length][1]).toFixed(2);
+        newCid[length][1] = +(newCid[length][1] - units[length][1]).toFixed(2);
+        result.change[length][1] = +(result.change[length][1] + units[length][1]).toFixed(2);
         }
     }
     if (change == 0) break;
     length--;
   }
 
-  result.change = result.change.filter(function(e) {
-    if (e[1] != 0) return true;
-  })
-
   if (change > 0) {
     result.status = "INSUFFICIENT_FUNDS"
-  } else {
-    const isClosed = newCid.findIndex(function(e){
-      if (e[1] != 0) return true;
+    result.change = [];
+  } else if (change == 0) {
+    let isOpen = false;
+    for (let i = 0; i < newCid.length; i++) {
+      if (newCid[i][1] != 0) {
+        isOpen = true;
+        break;
+      }
+    }
+    result.change.sort(function(a, b) {
+      if (a[1] === b[1]) {
+        return 0;
+        }
+      else {
+        return (a[1] > b[1]) ? -1 : 1;
+      }
     });
-    if (isClosed == true) {
-      result.status = "CLOSED";
-    } else {
+    if (isOpen == true) {
       result.status = "OPEN";
+      result.change = result.change.filter(function(e) {
+        if (e[1] != 0) return e;
+      })
+    } else {
+      result.status = "CLOSED";
     }
   }
 
